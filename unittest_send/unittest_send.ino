0ec5227ba573d500fb1f0daa32e760e9156b0e7e
TenzinCHW <tenzin_chan@mymail.sutd.edu.sg>
2017-08-09 16:38:36 +0800

    Added test code for sending

--- unittest_send/unittest_send.ino
+++ unittest_send/unittest_send.ino
@@ -14,29 +14,57 @@ int val = 0;
 // Variables to store time
 unsigned long time = 0;
 unsigned long last_sent = 0;
+int POD = 10;
 
 void setup() {
   // Start the serial ports ...
   Serial.begin( 9600 );
   Serial1.begin(9600);
 
-//  xbeeSerial.begin( 9600 );
+  //  xbeeSerial.begin( 9600 );
   // ... and set the serial port for the XBee radio.
   xbee.setSerial( Serial1 );
   // Receive TX Status packets
   xbee.setAcknowledgement(true);
   // The frame data in a ZigBee packet refers to the data between
   // the length LSB and the checksum.
-  uint8_t exFrame[] = { 0x10, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0xff, 0xff };
+  uint8_t exFrame[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
   // Now store the example frame in the packet object.
   zbp.setFrameData(0, exFrame, sizeof(exFrame));
   Serial.println("Ready to send");
 }
 
+uint8_t data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+char m;
+
 void loop() {
-  printPacket(zbp);
-  xbee.send(zbp);
+  while (Serial.available()) {
+    m = Serial.read();
+    switch (m) {
+      case 'a': POD = 1; break;
+      case 's': POD = 2; break;
+      case 'd': POD = 3; break;
+      case 'f': POD = 4; break;
+      case 'g': POD = 5; break;
+      case 'h': POD = 6; break;
+      case 'j': POD = 7; break;
+      case 'k': POD = 8; break;
+      case 'l': POD = 9; break;
+      case ';': POD = 10; break;
+      case '1': send_status(0); printPacket(zbp); break;
+      case '2': send_status(1); printPacket(zbp); break;
+      default: continue;
+    }
+  }
   delay(1000);
+}
+
+void send_status(uint8_t stat) {
+  data[POD - 1] = stat;
+  for (int i = 0; i < 10; i++) {
+    zbp.setFrameData(i, data[i]);
+  }
+  xbee.send(zbp);
 }
 
 void printPacket(SimpleZigBeePacket & p) {
@@ -53,7 +81,7 @@ void printPacket(SimpleZigBeePacket & p) {
     Serial.print(' ');
     checksum += p.getFrameData(i);
   }
-//   Calculate checksum based on summation of frame bytes
+  //   Calculate checksum based on summation of frame bytes
   checksum = 0xff - checksum;
   Serial.print(checksum, HEX );
   Serial.println();
